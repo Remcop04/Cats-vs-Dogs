0d9d2b9d3bdc871a82c3b9a92f4b63aab8c5d7ca
Remcop_laptop <remcopoelarends@live.nl>
2020-09-10 17:53:48 +0200

    .gitignore.txt gewijzigd

--- pre_process_photo_size.py
+++ pre_process_photo_size.py
@@ -1,36 +1,32 @@
-# -*- coding: utf-8 -*-
-"""
-Created on Thu Sep 10 15:41:23 2020
-
-@author: Remco
-"""
-
-# load dogs vs cats dataset, reshape and save to a new file
+# organize dataset into a useful structure
+from os import makedirs
 from os import listdir
-from numpy import asarray
-from numpy import save
-from keras_preprocessing.image import load_img
-from keras_preprocessing.image import img_to_array
-# define location of dataset
-folder = 'C:/Users/remco/Documents/GitHub/Cats-vs-Dogs/Dataset/train/'
-photos, labels = list(), list()
-# enumerate files in the directory
-for file in listdir(folder):
-	# determine class
-	output = 0.0
+from shutil import copyfile
+from random import seed
+from random import random
+# create directories
+dataset_home = 'dataset_dogs_vs_cats/'
+subdirs = ['train/', 'test/']
+for subdir in subdirs:
+	# create label subdirectories
+	labeldirs = ['dogs/', 'cats/']
+	for labldir in labeldirs:
+		newdir = dataset_home + subdir + labldir
+		makedirs(newdir, exist_ok=True)
+# seed random number generator
+seed(1)
+# define ratio of pictures to use for validation
+val_ratio = 0.25
+# copy training dataset images into subdirectories
+src_directory = 'C:/Users/remco/Documents/GitHub/Cats-vs-Dogs/Dataset/train/'
+for file in listdir(src_directory):
+	src = src_directory + '/' + file
+	dst_dir = 'train/'
+	if random() < val_ratio:
+		dst_dir = 'test/'
 	if file.startswith('cat'):
-		output = 1.0
-	# load image
-	photo = load_img(folder + file, target_size=(200, 200))
-	# convert to numpy array
-	photo = img_to_array(photo)
-	# store
-	photos.append(photo)
-	labels.append(output)
-# convert to a numpy arrays
-photos = asarray(photos)
-labels = asarray(labels)
-print(photos.shape, labels.shape)
-# save the reshaped photos
-save('dogs_vs_cats_photos.npy', photos)
-save('dogs_vs_cats_labels.npy', labels)
+		dst = dataset_home + dst_dir + 'cats/'  + file
+		copyfile(src, dst)
+	elif file.startswith('dog'):
+		dst = dataset_home + dst_dir + 'dogs/'  + file
+		copyfile(src, dst)
